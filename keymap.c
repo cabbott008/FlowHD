923d95196427b2992c93a4e0cc3294ef4ea097e3
cabbott008 <curtisabbott@me.com>
2024-10-28 23:22:39 -0500

    previous uncommitted updates

--- keymap.c
+++ keymap.c
@@ -8,12 +8,18 @@ enum custom_keycodes {
     NDASH,
     DVIDE,
     DGREE,
-    QMKLS,
     FRMWR,
     FLSH,
     R_QUE,
     R_EXC,
     R_XXX,
+    PT_RHT,
+    PT_LFT,
+    PT_DWN,
+    PT_UP,
+    PRAYER,
+    STOP,
+    CHILDX,
     SLCTALL,
     UNDO,
     COPY,
@@ -21,7 +27,7 @@ enum custom_keycodes {
     PASTE,
 };
 
-// Layer 0 = Basic Qwerty (Blue), Layer 1 = FunctionKeys & NumPad (Green), Layer 2 = Various Specialty Keys
+// Layer 0 = Basic Qwerty (Blue), Layer 1 = Various Symbol Keys (White), Layer 2 = Function Keys & Number Pad (Green)
 
 const uint16_t PROGMEM keymaps[] [MATRIX_ROWS] [MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
@@ -32,9 +38,9 @@ const uint16_t PROGMEM keymaps[] [MATRIX_ROWS] [MATRIX_COLS] = {
     KC_LSFT,            LCTL_T(KC_NO),      LALT_T(KC_LEFT),    LGUI_T(KC_RIGHT),   LCTL(KC_BSPC),                         TO(1),               TO(1),                                   KC_BSPC,            RCTL_T(KC_DOWN),    RALT_T(KC_UP),      RGUI_T(KC_NO),      KC_RSFT,
                                                                                     KC_SPC,             KC_ENTER,          KC_HOME,             KC_END,              LT(1, KC_TAB),      KC_SPC
   ),
-
-  [1] = LAYOUT_moonlander(
-    KC_NO,              KC_F9,              KC_F5,              KC_F1,              KC_F3,              KC_F7,             KC_F11,              KC_F12,              KC_F6,              KC_F2,              KC_F10,             KC_F4,              KC_F8,              KC_TRNS,
+// The left keypad is not used on Layer 1 to protect against strange behavior in using a multi-use key with the ' key.
+  [1] = LAYOUT_moonlander(:
+    KC_NO,              KC_NO,              KC_NO,              KC_NO,              KC_NO,              KC_NO,             KC_NO,               KC_NO,               KC_NO,              KC_NO,              KC_NO,              KC_NO,              KC_NO,              KC_TRNS,
     KC_TAB,             KC_NO,              KC_NO,              KC_NO,              KC_NO,              KC_NO,             KC_NO,               KC_NO,               LSFT(KC_7),         LSFT(KC_3),         LSFT(KC_1),         LSFT(KC_8),         ELPS,               LSFT(KC_6),
     KC_ESC,             KC_NO,              KC_NO,              KC_NO,              KC_NO,              KC_NO,             KC_NO,               KC_NO,               LSFT(KC_2),         LSFT(KC_MINUS),     MDASH,              NDASH,              KC_LBRC,            KC_RBRC,
     SC_LSPO,            KC_NO,              KC_NO,              KC_NO,              KC_NO,              KC_NO,                                                       LSFT(KC_4),         LSFT(KC_5),         KC_GRV,             LSFT(KC_GRV),       LSFT(KC_LBRC),      LSFT(KC_RBRC),
@@ -43,10 +49,10 @@ const uint16_t PROGMEM keymaps[] [MATRIX_ROWS] [MATRIX_COLS] = {
   ),
 
   [2] = LAYOUT_moonlander(
-    KC_NO,              KC_F9,              KC_F5,              KC_F1,              KC_F3,              KC_F7,             KC_F11,              KC_F12,              KC_F6,              KC_F2,              KC_F10,             KC_F4,              KC_F8,              KC_TRNS,
-    KC_TAB,             FLSH,               QMKLS,              KC_NO,              R_QUE,              R_EXC,             DGREE,               DVIDE,               KC_EQUAL,           KC_7,               KC_8,               KC_9,               KC_COMMA,           KC_BSLS,
-    KC_ESC,             UNDO,               SLCTALL,            CUT,                COPY,               PASTE,             KC_LBRC,             KC_RBRC,             KC_MINUS,           KC_4,               KC_5,               KC_6,               KC_SCLN,          KC_DOT,
-    SC_LSPO,            FRMWR,              R_XXX,              KC_NO,              KC_NO,              KC_NO,                                                       KC_0,               KC_1,               KC_2,               KC_3,               KC_SLASH,           KC_TRNS,
+    FLSH,               KC_F9,              KC_F5,              KC_F1,              KC_F3,              KC_F7,             KC_F11,              KC_F12,              KC_F6,              KC_F2,              KC_F10,             KC_F4,              KC_F8,              FRMWR,
+    KC_TAB,             PT_UP,              PRAYER,             STOP,               R_QUE,              R_EXC,             DGREE,               DVIDE,               KC_EQUAL,           KC_7,               KC_8,               KC_9,               KC_COMMA,           KC_BSLS,
+    KC_ESC,             UNDO,               SLCTALL,            CUT,                COPY,               PASTE,             KC_LBRC,             KC_RBRC,             KC_MINUS,           KC_4,               KC_5,               KC_6,               KC_SCLN,            KC_DOT,
+    SC_LSPO,            PT_DWN,             R_XXX,              PT_RHT,             PT_LFT,             CHILDX,                                                      KC_0,               KC_1,               KC_2,               KC_3,               KC_SLASH,           KC_TRNS,
     KC_LSFT,            LCTL_T(KC_NO),      LALT_T(KC_LEFT),    LGUI_T(KC_RIGHT),   LCTL(KC_DEL),                          TO(0),               TO(0),                                   KC_BSPC,            RCTL_T(KC_DOWN),    RALT_T(KC_UP),      RGUI_T(KC_NO),      KC_TRNS,
                                                                                     KC_SPC,             KC_ENTER,          KC_PGUP,             KC_PGDN,             KC_TAB,             KC_SPC
   ),
@@ -132,12 +138,6 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
   switch (keycode) {
-      case QMKLS:
-      if (record->event.pressed) {
-          SEND_STRING ("cd /home/ca/Sources/qmk_firmware");
-      }
-
-      break;
       case FRMWR:
       if (record->event.pressed) {
           SEND_STRING ("qmk compile -kb moonlander -km FlowHD");
@@ -254,6 +254,76 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
           SEND_STRING (SS_LCTL(SS_LSFT("u")));
           SEND_STRING ("2757");
+          tap_code16 (KC_SPC);
+          return false;
+      }
+      return true;
+
+      break;
+    case PT_LFT: // Hand pointing left
+      if (record->event.pressed) {
+          SEND_STRING (SS_LCTL(SS_LSFT("u")));
+          SEND_STRING ("1f448");
+          tap_code16 (KC_SPC);
+          return false;
+      }
+      return true;
+
+      break;
+    case PT_RHT: // Hand pointing right
+      if (record->event.pressed) {
+          SEND_STRING (SS_LCTL(SS_LSFT("u")));
+          SEND_STRING ("1f449");
+          tap_code16 (KC_SPC);
+          return false;
+      }
+      return true;
+
+      break;
+    case PT_UP: // Hand point up
+      if (record->event.pressed) {
+          SEND_STRING (SS_LCTL(SS_LSFT("u")));
+          SEND_STRING ("1f446");
+          tap_code16 (KC_SPC);
+          return false;
+      }
+      return true;
+
+      break;
+    case PT_DWN: // Hand point down
+      if (record->event.pressed) {
+          SEND_STRING (SS_LCTL(SS_LSFT("u")));
+          SEND_STRING ("1f447");
+          tap_code16 (KC_SPC);
+          return false;
+      }
+      return true;
+
+      break;
+   case PRAYER: // Praying hands
+      if (record->event.pressed) {
+          SEND_STRING (SS_LCTL(SS_LSFT("u")));
+          SEND_STRING ("1f64f");
+          tap_code16 (KC_SPC);
+          return false;
+      }
+      return true;
+
+      break;
+   case STOP: // Praying hands
+      if (record->event.pressed) {
+          SEND_STRING (SS_LCTL(SS_LSFT("u")));
+          SEND_STRING ("1f6d1");
+          tap_code16 (KC_SPC);
+          return false;
+      }
+      return true;
+
+      break;
+    case CHILDX: // Warning Children Crossing
+      if (record->event.pressed) {
+          SEND_STRING (SS_LCTL(SS_LSFT("u")));
+          SEND_STRING ("1f6b8");
           tap_code16 (KC_SPC);
           return false;
       }
